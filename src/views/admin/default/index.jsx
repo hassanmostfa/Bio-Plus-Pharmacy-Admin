6f881f0c2d2af918fc7db691067f33fa56314bb3
Hassan Mostafa <01092841138>
2025-03-02 21:12:34 +0200

    first commit

--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -8,30 +8,14 @@ import IconBox from "components/icons/IconBox";
 import MiniStatistics from "components/card/MiniStatistics";
 import {
   MdOutlineGroup,
-  MdOutlineLocalPharmacy,
-  MdOutlineMedicalServices,
-  MdOutlineStore,
   MdOutlineShoppingCart,
-  MdOutlineEventNote,
-  MdOutlinePerson,
-  MdOutlineBrandingWatermark,
+  MdAssignment,
 } from "react-icons/md";
-import TotalSpent from "views/admin/default/components/TotalSpent";
-import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
 import CheckTable from "views/admin/default/components/CheckTable";
-import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import PieCard from "views/admin/default/components/PieCard";
-import ComplexTable from "views/admin/default/components/ComplexTable";
-import Tasks from "views/admin/default/components/Tasks";
-import MiniCalendar from "components/calendar/MiniCalendar";
-import { columnsDataCheck, columnsDataComplex } from "views/admin/default/variables/columnsData";
+import { columnsDataCheck } from "views/admin/default/variables/columnsData";
 import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
-import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
 
-import { AiOutlineProduct } from "react-icons/ai";
 import { LuShoppingBasket } from "react-icons/lu";
-import { LuLayers3 } from "react-icons/lu";
-import { MdFamilyRestroom } from "react-icons/md";
 
 
 export default function UserReports() {
@@ -40,17 +24,9 @@ export default function UserReports() {
 
   const cardData = [
     { name: "Total Admins", value: "25", icon: MdOutlineGroup },
-    { name: "Total Pharmacy", value: "150", icon: MdOutlineLocalPharmacy },
-    { name: "Total Doctors", value: "75", icon: MdOutlineMedicalServices },
-    { name: "Total Clinics", value: "40", icon: MdOutlineStore },
     { name: "Total Orders", value: "5,235", icon: MdOutlineShoppingCart },
-    { name: "Total Appointments", value: "2,345", icon: MdOutlineEventNote },
-    { name: "Total Users", value: "10,000", icon: MdOutlinePerson },
-    { name: "Total Brands", value: "320", icon: MdOutlineBrandingWatermark },
     { name: "Total Products", value: "1,000", icon: LuShoppingBasket },
-    { name: "Total Product Types", value: "5", icon: AiOutlineProduct },
-    { name: "Total Categories", value: "100", icon: LuLayers3 },
-    { name: "Total Family Accounts", value: "5", icon: MdFamilyRestroom },
+    { name: "Total Prescriptions", value: "1,000", icon: MdAssignment },
   ];
 
   return (
